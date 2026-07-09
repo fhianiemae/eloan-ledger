37176121ecbfff38739158b943e730a3f46c8687
Hianie Mae Fernandez <fhianiemae@gmail.com>
2026-07-09 23:25:14 +0800

    Wire up Firebase project config

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,10 @@
 // secret: access to your data is controlled by Firebase Authentication + firestore.rules,
 // not by hiding these keys. See README.md for the full setup walkthrough.
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAlmAjghtX1YXi0zyN7GlZw_Z8MCYFApc0",
+  authDomain: "eloan-ledger.firebaseapp.com",
+  projectId: "eloan-ledger",
+  storageBucket: "eloan-ledger.firebasestorage.app",
+  messagingSenderId: "420250037230",
+  appId: "1:420250037230:web:5335f344660f9b48e3bcff"
 };
